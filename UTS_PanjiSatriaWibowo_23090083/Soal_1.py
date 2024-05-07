dd7613baac5ae3441f4cb5af71073b3cd56d1f80
panjisatria2 <149869811+panjisatria2@users.noreply.github.com>
2024-05-07 16:25:47 +0700

    Add files via upload

---
+++ UTS_PanjiSatriaWibowo_23090083/Soal_1.py
@@ -0,0 +1,6 @@
+bil_1 = int(input('Masukan Bilangan Pertama : '))
+bil_2 = int(input('Masukan Bilangan Ke2 : '))
+penjumlahan = bil_1 + bil_2
+pegurangan = bil_1 - bil_2
+modus = bil_1 % bil_2
+print([penjumlahan,pegurangan,modus])
---
+++ UTS_PanjiSatriaWibowo_23090083/Soal_3.py
@@ -0,0 +1,7 @@
+jumlah = int(input('Masukan jumlah Barang : '))
+barang_1 = int(input('Masukan Harga Barang Ke 1: '))
+barang_2 = int(input('Masukan Harga Barang Ke 2: '))
+harga_barang = barang_1 * jumlah
+harga_barang1 = barang_2 * jumlah
+total_harga = harga_barang + harga_barang1
+print(f'Total Belanjaan : {total_harga}')
